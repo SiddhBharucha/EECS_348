7ee72f7a5747d2c54912df9301f3142a5d499bde
SiddhBharucha <siddhbharucha@gmail.com>
2023-09-01 14:17:24 -0500

    replace the hello-world program with the sample C program

--- Labs/lab_01/tmp/test1/hello.c
+++ Labs/lab_01/tmp/test1/hello.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-    printf("%s\n", "bye world");
+int main() {
+    char str[100];
+    int i, len;
+
+    printf("Enter a string: ");
+
+    if (fgets(str, sizeof(str), stdin) != NULL) {
+        if (str[strlen(str) - 1] == '\n') {
+            str[strlen(str) - 1] = '\0';
+        }
+
+        len = strlen(str);
+
+        for (i = 0; i < len; i++) {
+            if (str[i] >= 'a' && str[i] <= 'z') {
+                str[i] = str[i] - 'a' + 'A';
+            }
+        }
+
+        printf("The capitalized string is: %s\n", str);
+
+        // Reverse the string.
+        for (i = 0, len = strlen(str); i < len / 2; i++) {
+            char temp = str[i];
+            str[i] = str[len - 1 - i];
+            str[len - 1 - i] = temp;
+        }
+
+        printf("The reversed string is: %s\n", str);
+    } else {
+        printf("Error reading input.\n");
+    }
+
     return 0;
 }
